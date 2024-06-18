90b72edd54f50912217719753decd718e0a38c06
Bestin John <bestinjohn1993@gmail.com>
2024-06-18 23:02:12 +0200

    Corrects Crawl URL

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https:bestinjohn.com',
+      url: 'https://bestinjohn.com',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
