c492791f8ca5bd9146618b81983445239854332c
Bestin John <bestinjohn1993@gmail.com>
2025-04-13 12:30:13 +0200

    feat: Replace SCSS with CSS modules for Introduction component and add manifest.json

--- src/app/_components/Introduction/Introduction.tsx
+++ src/app/_components/Introduction/Introduction.tsx
@@ -1,33 +1,33 @@
 import Image from "next/image";
-//import "./Introduction.scss";
 import { imgSiteBanner } from "~/app/_assets";
 import { api } from "~/trpc/server";
+import styles from "./Introduction.module.css";
 
 const Introduction = async () => {
   const experience = await api.portfolio.introduction();
 
   return (
-    <section id="introduction" className="introduction mb-10">
+    <section id="introduction" className="mb-10">
       <div className="container-fluid">
         <div className="flex flex-wrap">
-          <div className="intro-content relative z-0 px-4 lg:left-16 lg:mt-16 lg:px-0">
+          <div
+            className={`relative z-0 px-4 lg:left-16 lg:mt-16 lg:px-0 ${styles.introContent}`}
+          >
             <div className="relative mt-7 flex items-center lg:left-14">
-              <div className="intro-text text-xl">{experience.introText}</div>
-              <div className="intro-line relative flex-1">
-                <span className="line line-horizontal absolute top-0.5 right-0 left-2"></span>
+              <div className="max-w-[80%] text-xl">{experience.introText}</div>
+              <div className="relative flex-1">
+                <span className="absolute top-0.5 right-0 left-2 h-px bg-current opacity-50"></span>
               </div>
             </div>
-            <h1 className="site-title my-2">
+            <h1 className="my-2 text-3xl font-bold">
               <strong>{experience.name}</strong>
               {experience.caption}
             </h1>
-            <div className="intro-box bg-shadow relative mt-7 mb-4 rounded-sm p-7 lg:mb-0 lg:ml-7">
-              <p className="site-subtitle mb-7 max-w-lg text-xl">
-                {experience.description}
-              </p>
+            <div className="relative mt-7 mb-4 rounded-sm p-7 shadow-md lg:mb-0 lg:ml-7">
+              <p className="mb-7 max-w-lg text-xl">{experience.description}</p>
             </div>
           </div>
-          <div className="intro-img m-0">
+          <div className={`m-0 ${styles.introImg}`}>
             <Image
               src={imgSiteBanner}
               width={1200}
