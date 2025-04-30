a4a55239789166aae76493c3283e93d9de373652
Bestin John <bestinjohn1993@gmail.com>
2025-04-30 23:24:49 +0200

    feat: Add ChangeColorMode component with theme options and styles

--- src/app/_components/index.ts
+++ src/app/_components/index.ts
@@ -1,15 +1,15 @@
-import AboutMe from './AboutMe/AboutMe';
-import ChangeColorMode from './ChangeColorMode/ChangeColorMode';
-import Contact from './Contact/Contact';
-import ContactFormEmailTemplate from './Emails/ContactFormEmailTemplate';
-import Experience from './Experience/Experience';
-import Footer from './Footer/Footer';
-import GoToTop from './GoToTop/GoToTop';
-import Header from './Header/Header';
-import Introduction from './Introduction/Introduction';
-import Services from './Services/Services';
-import SocialLinks from './SocialLinks/SocialLinks';
-import Timeline from './Timeline/Timeline';
+import AboutMe from "./AboutMe/AboutMe";
+import ChangeColorMode from "./change-color-mode/ChangeColorMode";
+import Contact from "./Contact/Contact";
+import ContactFormEmailTemplate from "./Emails/ContactFormEmailTemplate";
+import Experience from "./Experience/Experience";
+import Footer from "./Footer/Footer";
+import GoToTop from "./GoToTop/GoToTop";
+import Header from "./Header/Header";
+import Introduction from "./Introduction/Introduction";
+import Services from "./Services/Services";
+import SocialLinks from "./SocialLinks/SocialLinks";
+import Timeline from "./Timeline/Timeline";
 
 export {
   AboutMe,
