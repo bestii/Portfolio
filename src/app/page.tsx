224b4cf18ac6a10d0f4c3c128e359dd3c5b61a50
Bestin John <bestinjohn1993@gmail.com>
2026-05-15 00:57:05 +0200

    feat: add Skills component to showcase technologies used

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Education from "@/components/education/Education";
 import Experience from "@/components/experience/Experience";
 import Footer from "@/components/footer/Footer";
 import Introduction from "@/components/introduction/Introduction";
+import Skills from "@/components/skills/Skills";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
         <AboutMe />
         <Experience />
         <Education />
+        <Skills />
         <Contact />
         <Footer />
       </div>
---
+++ src/components/skills/Skills.tsx
@@ -0,0 +1,5 @@
+const Skills = () => {
+  return <section className="max-w-225">Skills</section>;
+};
+
+export default Skills;
